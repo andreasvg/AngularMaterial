cc76c15f7cbb2fa51ef78ce3a5db5289830c6122
Andreas van Greunen <andreasvangreunen@iMac.lan>
2019-03-13 13:18:56 +0000

    Added data table

---
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { Note } from '../../models/note';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.scss']
+})
+export class NotesComponent implements OnInit, AfterViewInit {
+  @Input() notes: Note[];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  displayedColumns: string[] = ['position', 'title', 'date'];
+  dataSource: MatTableDataSource<Note>;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
+    this.dataSource.sort = this.sort;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(filterValue: string): void {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  onSortChange(event) {
+    console.log('sort changed');
+    console.log(event);
+  }
+}
